f1ed1c2bbf6237de4c029cd3c03ddf0770e37a93
ProSplash <yuraplonka@gmail.com>
2026-02-02 23:56:24 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.markdown("""
     
     /* FORCE MAX HEIGHT ON RESULT BOXES */
     div[data-testid="stAlert"] {
-        max-height: 46px !important;     /* Limits height to match standard buttons */
+        max-height: 20px !important;     /* Limits height to match standard buttons */
     }
     </style>
     """, unsafe_allow_html=True)
